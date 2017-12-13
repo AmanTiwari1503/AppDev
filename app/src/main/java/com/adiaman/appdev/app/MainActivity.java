4ebc15baef3e66d6d00573a0cee314025f08573a
AmanTiwari1503 <tiwariaman2000@gmail.com>
2017-12-13 16:07:23 +0530

    Initial commit

---
+++ app/src/main/java/com/adiaman/appdev/app/MainActivity.java
@@ -0,0 +1,29 @@
+package com.adiaman.appdev.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button counterBtn=(Button) findViewById(R.id.counterBtn);
+
+        counterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView displayTextView=(TextView) findViewById(R.id.displayTextView);
+                int num=Integer.parseInt(displayTextView.getText().toString());
+                num++;
+                displayTextView.setText(num+"");
+            }
+        });
+
+    }
+}
